3e9b0eb77fad031d600ee8aaf11bae6dd07d3fe9
Ritik Gupta <ritikgupta@Ritiks-MacBook-Air.local>
2025-09-22 14:59:58 +0530

    fix:deploy fix 13

--- saas-dashboard/vite.config.ts
+++ saas-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/juspay_dashboard_assesment/',
+  base:'./',
   resolve: {
     dedupe: ['@emotion/react', '@emotion/styled'],
   },
